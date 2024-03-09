a21c40fd489ba3556c42843722af0d217c2cd07e
Brazz26 <147454142+Brazz26@users.noreply.github.com>
2024-03-08 19:49:43 -0500

    created function with inquirer prompts using switch case for shape choice

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-
+const {Circle, Triangle, Square} = require('./lib/shapes');
+const Svg = require('./lib/svg');
 
 
 
 function makeLogo() {
-
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'text',
+        message: "Enter your Logo's text"
+    },
+    {
+        type: 'input',
+        name: 'textcolor',
+        message: 'What color would you like your text?'
+    },
+    {
+        type: 'input',
+        name: 'shapecolor',
+        message: 'What color would you like your shape?'
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like?',
+        choices: ['Circle', 'Triangle', 'Square']
+    }
+]).then((answers) => {
+    let shapeChoice;
+    switch(answers.shape) {
+        case 'Circle':
+            shapeChoice = new Circle()
+        break;
+        case 'Traingle':
+            shapeChoice = new Triangle()
+        break;
+        case 'Square':
+            shapeChoice = new Square()
+        break;
+    }
+    shapeChoice.setColor(answers.shapecolor)
+    const newSvg = new Svg()
+    newSvg.setShape(shapeChoice)
+    newSvg.setText(answers.text, answers.textcolor)
+    answers.text.length > 3? console.log('Must be no more than 3 characters'): fs.writeFileSync('my_logo.svg', newSvg.render())
+})
 }
 
 makeLogo();
